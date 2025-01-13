f8f9687d925bfed894060ecf830aee08e49457cf
limewing <58874904+limewing@users.noreply.github.com>
2025-01-13 16:59:50 +0900

    Add files via upload

---
+++ running_lv1-1.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i <= 100; i++)//for문
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+int j = 1;
+while (j <= 100)
+{
+    if (j % 2 == 1)
+        Console.WriteLine(j);
+    j++;
+}
+
+int k = 0;
+do
+{
+    if (k % 2 == 1)
+        Console.WriteLine(k);
+    k++;
+} while (k <= 100);
