0cd030fff025f70a8981b044ab20b992bb8b0766
limewing <58874904+limewing@users.noreply.github.com>
2025-01-13 17:16:36 +0900

    Add files via upload

---
+++ running_lv1-2.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = { 28, 29, 30, 31, 32 };
+int sum = 0;
+
+//sum
+for (int i = 0; i < 5; i++)
+    sum += numbers[i];
+
+//average
+float average = sum / 5;
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Average: {average}");
